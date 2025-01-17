74c3577c6df4ea4f47e08a323965175be16783fc
Rose Nedder <rose.nedder@gmail.com>
2025-01-17 14:12:21 -0500

    Express Server Part 2

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const app = express(); // returns an express server application under app
+// returns an express server application under app
+const app = express(); 
+
+// log using development version & will log request headers
+app.use(morgan('dev')); 
+
+// serves files from the public folder
+// automatically serves index.html if you just request the host name
+// __dirname is a special variable in Node - refers to the absolute path of current directory of the file it is in
+app.use(express.static(__dirname + '/public')); 
 
 // return a status 200 with the html code below for any request
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
